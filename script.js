3cd969b1b80c751560f3ca77590f972b76cf082a
Ritam acharya <ritamacharya2004@gmail.com>
2024-09-29 20:03:54 +0530

    add links and references

---
+++ script.js
@@ -0,0 +1,185 @@
+
+const s1 = document.getElementById("s1");
+s1.addEventListener("click" , () => {
+    window.location.href =  "https://signup.zerodha.com/";
+});
+
+
+const about = document.getElementById("about");
+about.addEventListener("click" , () => {
+    window.location.href =  "https://zerodha.com/about/";
+});
+
+
+const products = document.getElementById("products");
+products.addEventListener("click" , () => {
+    window.location.href =  "https://zerodha.com/products/";
+});
+
+const pricing = document.getElementById("pricing");
+pricing.addEventListener("click" , () => {
+    window.location.href =  "https://zerodha.com/charges/#tab-equities";
+});
+
+const support = document.getElementById("support");
+support.addEventListener("click" , () => {
+    window.location.href =  "https://support.zerodha.com/";
+});
+
+
+
+const signupForFree = document.getElementById("signup");
+signupForFree.addEventListener("click" , () => {
+    window.location.href =  "https://zerodha.com/open-account";
+});
+
+
+
+const exploreProducts = document.getElementById("ExploreProducts");
+exploreProducts.addEventListener("click" , () => {
+    window.location.href =  "https://zerodha.com/products"; 
+});
+
+
+
+const bdlogo = document.getElementById("bdlogo");
+bdlogo.addEventListener("click" , () => {
+    window.location.href =  "https://zerodha.com/media"; 
+});
+
+
+
+const kite = document.getElementById("kite");
+kite.addEventListener("click" , () => {
+    window.location.href =  "https://kite-demo.zerodha.com/dashboard"; 
+});
+
+
+const x = document.getElementById("x");
+x.addEventListener("click" , ()=> {
+    window.location.href = "https://x.com/zerodhaonline"
+});
+
+
+const fb = document.getElementById("facebook");
+fb.addEventListener("click" , ()=> {
+    window.location.href = "https://www.facebook.com/zerodha.social"
+});
+
+
+
+const insta = document.getElementById("insta");
+insta.addEventListener("click" , ()=> {
+    window.location.href = "https://www.instagram.com/zerodhaonline/"
+});
+
+
+
+const linkedin = document.getElementById("in");
+linkedin.addEventListener("click" , ()=> {
+    window.location.href = "https://www.linkedin.com/company/zerodha"
+});
+
+
+
+const yt = document.getElementById("yt");
+yt.addEventListener("click" , ()=> {
+    window.location.href = "https://www.youtube.com/@zerodhaonline"
+});
+
+
+
+const wp = document.getElementById("whatsapp");
+wp.addEventListener("click" , ()=> {
+    window.location.href = "https://www.whatsapp.com/channel/0029Va8tzF0EquiIIb9j791g"
+});
+
+
+
+const tele = document.getElementById("telegram");
+tele.addEventListener("click" , ()=> {
+    window.location.href = "https://t.me/zerodhain"
+});
+
+
+
+
+
+const varsity = document.getElementById("varsity");
+varsity.addEventListener("click" , ()=> {
+    window.location.href = "https://zerodha.com/varsity/"
+});
+
+
+
+
+const trading = document.getElementById("trading");
+trading.addEventListener("click" , ()=> {
+    window.location.href = "https://tradingqna.com/"
+});
+
+
+
+const pps = document.getElementById("pps");
+pps.addEventListener("click" , ()=> {
+    window.location.href = "https://zerodha.com/policies-and-procedures/"
+});
+
+
+
+
+
+const nse = document.getElementById("nse");
+nse.addEventListener("click" , ()=> {
+    window.location.href = "https://www.nseindia.com/"
+});
+
+
+
+const bse = document.getElementById("bse");
+bse.addEventListener("click" , ()=> {
+    window.location.href = "https://www.bseindia.com/"
+});
+
+
+
+const mcx = document.getElementById("mcx");
+mcx.addEventListener("click" , ()=> {
+    window.location.href = "https://www.mcxindia.com/"
+});
+
+
+
+
+const tc = document.getElementById("tc");
+tc.addEventListener("click" , ()=> {
+    window.location.href = "https://zerodha.com/terms-and-conditions/"
+});
+
+
+const pp = document.getElementById("pp");
+pp.addEventListener("click" , ()=> {
+    window.location.href = "https://zerodha.com/privacy-policy/"
+});
+
+
+
+const disclosure = document.getElementById("disclosure");
+disclosure.addEventListener("click" , ()=> {
+    window.location.href = "https://zerodha.com/disclosure/"
+});
+
+
+
+
+const investor = document.getElementById("investor");
+investor.addEventListener("click" , ()=> {
+    window.location.href = "https://zerodha.com/investor-attention/"
+});
+
+
+
+const investerCharacter = document.getElementById("investerCharacter");
+investerCharacter.addEventListener("click" , ()=> {
+    window.location.href = "https://zerodha.com/tos/investor-charter/"
+}); 
